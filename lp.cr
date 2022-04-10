48e7ad1713ac7cabe7aec8da031a447053c4db12
lucatake <luacatake@LuaCatake.localdomain>
2022-04-10 17:26:15 -0300

    instruções de controle exemplos

--- lp.cr
+++ lp.cr
@@ -39,6 +39,52 @@ def controlInstructions
   puts idade <= 21 ? (idade < 21 ? "Menor de 21 anos." : "21 anos.") : "Maior de 21 anos."
 
   puts "--------Unless---------"
+  unless idade == 21
+    puts "Idade diferente de 21 anos."
+  else
+    puts "21 anos."
+  end
+
+  puts "----------&&-----------"
+  result = idade == 21 && "21 anos."
+  puts result
+
+  puts "----------||-----------"
+  result = idade != 21 || "21 anos."
+  puts result
+
+  puts "---------Case----------"
+  case idade
+  when 21
+    puts "21 anos."
+  else
+    puts "Idade diferente de 21 anos.."
+  end
+  
+  puts "--------While---------"
+  idade += 1
+  while idade > 17
+    idade -= 1    
+    # sair do loop
+    if idade == 18
+      break
+    end
+    # tentar executar a próxima iteração do loop
+    if idade == 20
+      next
+    end
+    print "Idade "
+    print idade
+    print "\n"
+  end
+
+  puts "--------Until---------"
+  idade = 21
+  until idade < 20
+    puts "Idade #{idade.to_s}"
+    idade -= 1
+  end
+
 end
 
 end
